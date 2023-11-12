dfef65bcdfe2cd29d1c7069736934646d8153940
deanuharatinu <deanu.alt@gmail.com>
2023-11-12 23:21:44 +0700

    add barchart animation

---
+++ app/src/main/java/com/deanuharatinu/barchart/BarChart.kt
@@ -0,0 +1,115 @@
+package com.deanuharatinu.barchart
+
+import android.animation.ObjectAnimator
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.animation.DecelerateInterpolator
+import android.widget.ProgressBar
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.deanuharatinu.barchart.databinding.BarLayoutBinding
+import com.deanuharatinu.barchart.databinding.ItemBarChartBinding
+
+class BarChart : ConstraintLayout {
+    private lateinit var binding: BarLayoutBinding
+    private val barChartViewList: MutableList<ItemBarChartBinding> = mutableListOf()
+
+    constructor(
+        context: Context
+    ) : super(context) {
+        init()
+    }
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet?
+    ) : super(context, attrs) {
+        init()
+    }
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyle: Int
+    ) : super(
+        context,
+        attrs,
+        defStyle
+    ) {
+        init()
+    }
+
+    private fun init() {
+        binding = BarLayoutBinding.inflate(LayoutInflater.from(context), this, true)
+
+        barChartViewList.add(binding.line1)
+        barChartViewList.add(binding.line2)
+        barChartViewList.add(binding.line3)
+        barChartViewList.add(binding.line4)
+        barChartViewList.add(binding.line5)
+        barChartViewList.add(binding.line6)
+        barChartViewList.add(binding.line7)
+    }
+
+    private fun startAnimation(target: ProgressBar, value: Float, maxValue: Float) {
+        if (maxValue < value) return
+
+        val calculatedValue = (100 / maxValue) * value
+
+        val objAnim = ObjectAnimator.ofInt(
+            target,
+            "progress",
+            0,
+            calculatedValue.toInt()
+        ).apply {
+            duration = 500
+            startDelay = 200
+            interpolator = DecelerateInterpolator()
+
+        }
+        objAnim.start()
+    }
+
+    fun setValue(
+        barChartList: List<BarChartModel>,
+        onClick: ((Int, View) -> Unit)? = null
+    ) {
+        var calculatedMaxValue = 0f
+
+        barChartList.forEachIndexed { index, barChartModel ->
+            if (index < barChartViewList.size) {
+                calculatedMaxValue = if (barChartModel.maxValue > calculatedMaxValue) {
+                    barChartModel.maxValue
+                } else {
+                    calculatedMaxValue
+                }
+
+                val itemBinding = barChartViewList[index]
+                itemBinding.tvDesc.text = barChartModel.title
+                startAnimation(
+                    itemBinding.bar,
+                    barChartModel.value,
+                    barChartModel.maxValue
+                )
+
+                itemBinding.bar
+                    .setOnClickListener { view ->
+                    onClick?.let { onClick(index, view) }
+                }
+            }
+        }
+
+        calculatedMaxValue /= 1_000_000f
+        binding.topHorizontalLine.tvHorizontalLineDesc.text = "${calculatedMaxValue}Jt"
+        binding.midHorizontalLine.tvHorizontalLineDesc.text =
+            "${(calculatedMaxValue / 2)}Jt"
+        binding.bottomHorizontalLine.tvHorizontalLineDesc.text = "0Jt"
+    }
+}
+
+data class BarChartModel(
+    val title: String,
+    val value: Float,
+    val maxValue: Float,
+)
--- app/src/main/java/com/deanuharatinu/barchart/MainActivity.kt
+++ app/src/main/java/com/deanuharatinu/barchart/MainActivity.kt
@@ -1,35 +1,49 @@
 package com.deanuharatinu.barchart
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.view.animation.Animation
-import android.view.animation.DecelerateInterpolator
-import android.view.animation.ScaleAnimation
 import androidx.appcompat.app.AppCompatActivity
 import com.deanuharatinu.barchart.databinding.ActivityMainBinding
-
+import com.skydoves.balloon.ArrowPositionRules
+import com.skydoves.balloon.Balloon
+import com.skydoves.balloon.BalloonAnimation
+import com.skydoves.balloon.BalloonSizeSpec
+import com.skydoves.balloon.showAlignTop
 
 class MainActivity : AppCompatActivity() {
-  private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    binding = ActivityMainBinding.inflate(layoutInflater)
-    setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    val scaleAnim = ScaleAnimation(
-      1f, 1f,
-      0f, 1f,
-      Animation.ABSOLUTE, 0f,
-      Animation.RELATIVE_TO_PARENT, 1f,
-    )
-    scaleAnim.duration = 600
-    scaleAnim.interpolator = DecelerateInterpolator()
-    scaleAnim.fillAfter = true
-    scaleAnim.fillBefore = false
-    scaleAnim.isFillEnabled = true
+        val barChartModels: MutableList<BarChartModel> = mutableListOf(
+            BarChartModel("Sen", 50_000f, 1_000_000f),
+            BarChartModel("Sel", 200_000f, 1_000_000f),
+            BarChartModel("Rab", 123_000f, 1_000_000f),
+            BarChartModel("Kam", 500_000f, 1_000_000f),
+            BarChartModel("Jum", 691_000f, 1_000_000f),
+            BarChartModel("Sab", 804_000f, 1_000_000f),
+            BarChartModel("Min", 630_000f, 1_000_000f),
+        )
 
-    binding.front.startAnimation(scaleAnim)
-  }
+        binding.barChart.setValue(barChartModels) { index, view ->
+            val balloon = Balloon.Builder(this)
+                .setLayout(R.layout.item_baloon)
+                .setTextColorResource(R.color.black)
+                .setElevation(6)
+                .setTextSize(15f)
+                .setArrowPositionRules(ArrowPositionRules.ALIGN_ANCHOR)
+                .setArrowSize(10)
+                .setArrowPosition(0.5f)
+                .setCornerRadius(8f)
+                .setBackgroundColorResource(R.color.white)
+                .setBalloonAnimation(BalloonAnimation.ELASTIC)
+                .setLifecycleOwner(this)
+                .build()
+
+            // TODO: valuenya sekarang berapa?
+            view.showAlignTop(balloon, yOff = bin)
+        }
+    }
 }
